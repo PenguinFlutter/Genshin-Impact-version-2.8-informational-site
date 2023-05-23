2de390b8dc988f9bdbc61bfcf189427ae19c7536
Ethan Park <129027018+PenguinFlutter@users.noreply.github.com>
2023-05-23 15:36:04 -0700

    Add files via upload

---
+++ Codes.js
@@ -0,0 +1,31 @@
+var Name = "Kazuha"
+function displayKazuha() {
+    document.getElementById("Intro").innerHTML= "Place Description of Kazuha here"
+    document.getElementById("CharTitle").innerHTML= "KADEHARA KAZUHA"
+    document.getElementById('CharIMG').src = "KazuhaTrans.png"
+    document.getElementById('CharIMG').style.marginRight = "3cm"
+    document.getElementById('CharIMG').style.width = "50%"
+    Name = "Kazuha"
+
+}
+
+function displayKlee() {
+    document.getElementById("Intro").innerHTML= "Place Description of Klee here"
+    document.getElementById("CharTitle").innerHTML= "KLEE"
+    document.getElementById('CharIMG').src = "KleeTrans.png"
+    document.getElementById('CharIMG').style.marginRight = "4cm"
+    document.getElementById('CharIMG').style.width = "75%"
+    Name = "Klee"
+
+
+}
+
+function displayYoimiya() {
+    document.getElementById("Intro").innerHTML= "Place Description of Yoimiya here"
+    document.getElementById("CharTitle").innerHTML= "YOIMIYA"
+    document.getElementById('CharIMG').src = "YoimiyaTrans.png"
+    document.getElementById('CharIMG').style.marginRight = "5cm"
+    document.getElementById('CharIMG').style.width = "100%"
+    document.getElementById('CharIMG').style.marginTop = "2cm"
+    Name = "Yoimiya"
+}
